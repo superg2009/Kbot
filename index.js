20d9b8d0c715b4cde4c17409cf6b4149e0baf0d0
Grant Marshall <grantmarshall@Grants-Mac-mini.local>
2018-08-19 13:53:21 -0700

    added stalememes and a touch of formatting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file=> file.endsWith('.js'));
 
-for(const file of commandFiles){
+for(const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name,command);
 }
